df92acf55181a78e9d112bc21b24e1a1f632207d
Sohag Desai <sohag.desai@gmail.com>
2021-09-10 14:23:23 -0700

    Added createVPC script. Initial implementation of read_EC2.

--- cloudQuest.py
+++ cloudQuest.py
@@ -1,49 +1,75 @@
-import boto3
 from boto3 import Session
-import os.path
-from os import path
+import os
 import sys
-import read_infra
+import pprint
+
+from read_infra import readEC2
 import write_infra
 
+INPUT="input"
+TARGET="target"
+DEPLOYMENTS = {
+    INPUT: None,
+    TARGET: None
+}
+
+class Deployment:
+    def __init__(self, profile_type):
+        self.profile = input("Enter profile name of " + profile_type + " AWS deployment: ")
+        self.region = input("Enter region of " + profile_type + " AWS deployment: ")
+        os.environ['AWS_DEFAULT_REGION'] = self.region
+
+    def set_access_key (self, access_key):
+        self.access_key = access_key
+
+    def set_secret_key (self, secret_key):
+        self.secret_key = secret_key
+
+
+def get_credentials(deployment_type):
+    DEPLOYMENTS[deployment_type] = Deployment(deployment_type)
+    try:
+        session = Session(profile_name=DEPLOYMENTS[deployment_type].profile)
+        credentials = session.get_credentials()
+        # Credentials are refreshable, so accessing your access key / secret key
+        # separately can lead to a race condition. Use this to get an actual matched
+        # set.
+        credentials = credentials.get_frozen_credentials()
+        DEPLOYMENTS[deployment_type].set_access_key(credentials.access_key)
+        DEPLOYMENTS[deployment_type].set_secret_key(credentials.secret_key)
+
+    except:
+        sys.exit("Error: invalid " + deployment_type + " profile.")
+
+    if not DEPLOYMENTS[deployment_type].region:
+        sys.exit("Error: invalid " + deployment_type + " region.")
+
+
 if __name__ == "__main__":
-    session = Session(profile_name='input')
-    os.environ['AWS_DEFAULT_REGION'] = 'us-east-1'
-    credentials = session.get_credentials()
-    # Credentials are refreshable, so accessing your access key / secret key
-    # separately can lead to a race condition. Use this to get an actual matched
-    # set.
-    current_credentials = credentials.get_frozen_credentials()
+    for deployment_type in DEPLOYMENTS.keys():
+        get_credentials(deployment_type)
+        # I would not recommend actually printing these. Generally unsafe.
+        print(deployment_type + " profile access key: " + DEPLOYMENTS[deployment_type].access_key)
+        print(deployment_type + " profile secret key: " + DEPLOYMENTS[deployment_type].secret_key)
+        print(deployment_type + " profile region: " + DEPLOYMENTS[deployment_type].region)
 
-    # I would not recommend actually printing these. Generally unsafe.
-    print("Input profile access key: " + current_credentials.access_key)
-    print("Input profile secret key: " + current_credentials.secret_key)
-    print("Input profile region: " + session.region_name)
+    ec2_description = readEC2.read_EC2(DEPLOYMENTS[INPUT])
+    ec2_reservations = ec2_description['Reservations']
+    num_reservations = len(ec2_reservations)
+    ec2_groups = []
+    ec2_instances = []
+    num_instances = 0
+    # breakpoint()
 
-    session = Session(profile_name='output')
-    os.environ['AWS_DEFAULT_REGION'] = 'us-west-1'
-    credentials = session.get_credentials()
-    # Credentials are refreshable, so accessing your access key / secret key
-    # separately can lead to a race condition. Use this to get an actual matched
-    # set.
-    current_credentials = credentials.get_frozen_credentials()
+    for idx in range(num_reservations):
+        ec2_groups.append(ec2_reservations[idx]['Groups'])
+        ec2_instances.append(ec2_reservations[idx]['Instances'])
 
-    # I would not recommend actually printing these. Generally unsafe.
-    print("Output profile access key: " + current_credentials.access_key)
-    print("Output profile secret key: " + current_credentials.secret_key)
-    print("Output profile region: " + session.region_name)
+    num_instances = len(ec2_instances)
 
-
-    # session = botocore.session.get_session()
-    # print(session.get_credentials().access_key)
-    # print(session.get_credentials().secret_key)
-    # input_path  = input("Enter pathname of source AWS deployment: ")
-    # if not path.exists(input_path):
-    #    sys.exit ("Error: invalid pathname for input credentials file.")
-    #
-    # output_path = input("Enter pathname of target AWS deployment: ")
-    # if not path.exists(output_path):
-    #    sys.exit ("Error: invalid pathname for target credentials file.")
-    #
-    # print("Input path: " + input_path)
-    # print("Output path: " + output_path)
+    print ("No of EC2 instance reservations found: " + str(num_instances))
+    print ("=================================================")
+    pp = pprint.PrettyPrinter(indent=2)
+    for instance in ec2_instances:
+        pp.pprint(instance)
+        print("=================================================")
--- read_infra/readEC2.py
+++ read_infra/readEC2.py
@@ -1,5 +1,14 @@
 import boto3
+import pprint
 
-ec2 = boto3.client('ec2')
-response = ec2.describe_instances()
-print(response)
+def read_EC2(profile):
+    ec2_client = boto3.client(
+        'ec2',
+        aws_access_key_id=profile.access_key,
+        aws_secret_access_key=profile.secret_key,
+        region_name = profile.region
+    )
+    response = ec2_client.describe_instances()
+    # pp = pprint.PrettyPrinter(indent=2)
+    # pp.pprint(response)
+    return response
